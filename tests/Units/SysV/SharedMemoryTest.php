67e8c81ff4a99609bd93b5c5575e53c9da92a537
takuya <55338+takuya@users.noreply.github.com>
2025-03-06 01:40:41 +0900

    revised

--- tests/Units/SysV/SharedMemoryTest.php
+++ tests/Units/SysV/SharedMemoryTest.php
@@ -8,21 +8,6 @@ use function Takuya\Helpers\str_rand;
 
 class SharedMemoryTest extends TestCase {
   
-  protected int $shm_cnt;
-  protected int $sem_cnt;
-  
-  protected function setUp():void {
-    parent::setUp();
-    $this->shm_cnt = `ipcs -m | wc -l `;
-    $this->sem_cnt = `ipcs -s | wc -l `;
-  }
-  
-  protected function tearDown():void {
-    parent::tearDown();
-    $this->assertEquals($this->shm_cnt, `ipcs -m | wc -l `);
-    $this->assertEquals($this->sem_cnt, `ipcs -s | wc -l `);
-  }
-  
   public function test_sysv_shared_memory_simple_var() {
     $key = rand(1, 100);
     $str = str_rand(10);
@@ -35,13 +20,13 @@ class SharedMemoryTest extends TestCase {
     $shm->destroy();
     $this->assertFalse($exists);
     $this->assertTrue($empty);
-    $this->assertEquals($str,$ret);
+    $this->assertEquals($str, $ret);
   }
   
   public function test_sysv_shared_memory_object_var() {
     $key = rand(1, 100);
     $obj = new \stdClass();
-    $obj->name =  str_rand(10);
+    $obj->name = str_rand(10);
     $shm = new IPCSharedMem(str_rand(10));
     $shm->put($obj);
     $ret = $shm->get();
@@ -51,6 +36,6 @@ class SharedMemoryTest extends TestCase {
     $shm->destroy();
     $this->assertFalse($exists);
     $this->assertTrue($empty);
-    $this->assertEquals($obj,$ret);
+    $this->assertEquals($obj, $ret);
   }
 }
